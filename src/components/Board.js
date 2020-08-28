a44522b305e6c5af69c89129ed4e287b09abbc4c
Ojhal Patwa <patkarojhal26@gmail.com>
2020-08-28 22:29:15 +0530

    add Board component

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Square from './Square';
+
+export default class Board extends Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+}
