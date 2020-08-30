d3a2ddb340019b671f6dd2f412038ea818de9d7f
Ojhal Patwa <patkarojhal26@gmail.com>
2020-08-30 14:35:13 +0530

    add css

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,6 +11,22 @@ export default class Board extends Component {
   render() {
     return (
       <div>
+        <div className="border-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="border-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="border-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+
 
       </div>
     )
