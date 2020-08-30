e21fc7f6d901c4e8b7c14337936ef90d64098797
Ojhal Patwa <patkarojhal26@gmail.com>
2020-08-30 22:24:46 +0530

    add some functionality

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import Board from './Board';
 
 export default class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      xIsNext: true,
+      stepNumber: 0,
+      history: [
+        { squares: Array(9).fill(null) }
+      ]
 
     }
   }
   render() {
     return (
-      <div>
+      <div className="game">
+        <div className="game-board">
+          <Board onClick={(i) => this.onClick(i)}
+            squares={current.squares} />
+
+        </div>
       </div>
     )
   }
