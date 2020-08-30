d2e675e7e7ca0edb3ea3037682f492e8f5c50e47
Ojhal Patwa <patkarojhal26@gmail.com>
2020-08-30 22:14:21 +0530

    create Constructor

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div>
