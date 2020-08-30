f05dae8f8e852e8a423a0d199355043445362f2a
Ojhal Patwa <patkarojhal26@gmail.com>
2020-08-30 14:25:44 +0530

    use props in class component

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Square from './Square';
 
 export default class Board extends Component {
+  renderSquare(i) {
+    return <Square value={this.props.square[i]}
+      onClick={() => this.props.onClick(i)}
+    />
+  }
 
   render() {
     return (
