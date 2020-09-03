c6fd980677dae96c3fffe530983186580dcb4414
Ojhal Patwa <patkarojhal26@gmail.com>
2020-09-03 22:43:59 +0530

    add for loop

--- src/components/Game.js
+++ src/components/Game.js
@@ -14,7 +14,17 @@ export default class Game extends Component {
     }
   }
   handleClick(i) {
-    const history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : '0';
+    this.setState({
+      history: history.concat({
+        squares: squares
+      }),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
+    });
   }
 
 
@@ -33,5 +43,21 @@ export default class Game extends Component {
         </div>
       </div>
     )
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
   }
 }
